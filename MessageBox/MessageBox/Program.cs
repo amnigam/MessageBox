280496b992a1fe31baf8ee434eb72490bef69ee4
User <User@WINDEV2407EVAL>
2024-10-30 15:38:16 -0700

    Add project files.

---
+++ MessageBox/MessageBox/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime.InteropServices; 
+
+namespace MessageBox
+{
+    internal class Program
+    {
+        [DllImport("user32.dll")]
+        public static extern int MessageBox(IntPtr hWnd, string lpText, string lpCaption, uint uType); 
+        static void Main(string[] args)
+        {
+            MessageBox(IntPtr.Zero, "Hello.. We are here!", "Sampark", 0); 
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Practicing with Windows APIs using C#
+
+Repository meant to learn about Windows APIs and their manipulation by using C# primarily.
